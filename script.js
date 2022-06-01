798ae0380a2eb2cc1cf29bff891792ce1a094bab
SahuPrachi <prachisahu998@gmail.com>
2022-06-01 22:35:51 +0530

    js started

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-"use strict";
+'use strict';
+
+//smooth scrolling
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
